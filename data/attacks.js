cf6c1a1682219fad6860d0e7568db28fd8d532e8
Rafael Lindemberg <rafalindemberg12@gmail.com>
2024-12-10 08:24:09 -0400

    first commit

---
+++ data/attacks.js
@@ -0,0 +1,22 @@
+const attacks = {
+  Tackle: {
+    name: 'Tackle',
+    damage: 10,
+    type: 'Normal',
+    color: 'black'
+  },
+  Fireball: {
+    name: 'Fireball',
+    damage: 25,
+    burning: false,
+    burn: 2,
+    type: 'Fire',
+    color: 'red'
+  },
+  WaterBall: {
+    name: 'Waterball',
+    damage: 15,
+    type: 'Water',
+    color: 'blue'
+  }
+}
